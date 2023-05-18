30397e4c8f561200b080256e728934466f14d7a7
iamgoeldhruv <122205781+iamgoeldhruv@users.noreply.github.com>
2023-05-18 22:47:50 +0530

    Add files via upload

---
+++ headerscript.js
@@ -0,0 +1,17 @@
+{/* <script>
+        function myfunction()
+        {
+           const a=document.querySelector(".side-menu");
+           console.log(5)
+           console.log(a.style.display);
+           console.log(dislay)
+           if(display=="block")
+           {
+            a.style.display="none";
+           }
+           if(display=="none")
+           {
+            a.style.display="block";
+           }
+        }
+     </script> */}
